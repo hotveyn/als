545363f5464a7ee37d3fad22b941a0ac0ab63391
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2024-05-03 18:57:26 +0300

    asd

--- server/api/group/.get.ts
+++ server/api/group/.get.ts
@@ -7,10 +7,5 @@ export interface ProductGroup {
 }
 
 export default defineEventHandler(async (): Promise<Group[]> => {
-  return await prisma.$queryRaw<Group[]>`
-    SELECT DISTINCT g.*
-    FROM groups g
-    INNER JOIN products p on p.group_id = g.id
-    ORDER BY created_at DESC
-  `;
+  return await prisma.group.findMany();
 });
